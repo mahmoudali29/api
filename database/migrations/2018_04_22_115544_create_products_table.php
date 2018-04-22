df81b3156bffb9aef1215fe6cd3b9cb5f5e89193
Mahmoud Ali <mahmoud.ali.29992@gmail.com>
2018-04-22 20:36:33 +0200

    Seeding part of Product and Review

--- database/migrations/2018_04_22_115544_create_products_table.php
+++ database/migrations/2018_04_22_115544_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('details');
+            $table->text('detail');
             $table->integer('price');
             $table->integer('stock');
             $table->integer('discount');
